3972a98ed7b687686701c3b9f000d30728babbb8
ali <alioliaee31@gmail.com>
2023-08-05 22:32:43 +0330

    add bottom tab navigator

---
+++ src/assets/icons/searchIconFilled.tsx
@@ -0,0 +1,11 @@
+import {SvgProps, SvgXml} from 'react-native-svg'
+
+const SearchIconFilled: React.FC<SvgProps> = (props) => {
+    const xml = `<svg width="21" height="21" viewBox="0 0 21 21" fill="white" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="9.76659" cy="9.76657" r="8.98856" stroke="#A7A7A7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16.0183 16.4851L19.5423 20" stroke="#A7A7A7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`
+    return <SvgXml xml={xml} {...props} />
+}
+
+export default SearchIconFilled
---
+++ src/navigation/bottom-tab.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {HomeScreen} from '../screens';
+import {Colors} from '../theme/colors';
+import styles from './bottom-tab-styles';
+import HomeIcon from '../assets/icons/homeIcon';
+import HeartIcon from '../assets/icons/heartIcon';
+import SearchIcon from '../assets/icons/searchIcon';
+import {TouchableOpacity, View} from 'react-native';
+import ProfileIcon from '../assets/icons/profileIcon';
+import CategoryIcon from '../assets/icons/categoryIcon';
+import HomeIconFilled from '../assets/icons/homeIconFilled';
+import HeartIconFilled from '../assets/icons/heartIconFilled';
+import ProfileIconFilled from '../assets/icons/profileIconFilled';
+import CategoryIconFilled from '../assets/icons/categoryIconFilled';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTab: React.FC = () => {
+  const screenOptions = {
+    headerShown: false,
+    tabBarLabel: '',
+    tabBarStyle: {
+      backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      borderTopWidth: 0,
+      position: 'absolute',
+      bottom: 20,
+      height: 60,
+    },
+  };
+
+  const iconRenderer = ({active, name}: {name: string; active: boolean}) => {
+    return name === 'Home' ? (
+      active ? (
+        <HomeIconFilled />
+      ) : (
+        <HomeIcon />
+      )
+    ) : name === 'Search' ? (
+      <SearchIcon color={active ? Colors.white : Colors.black} />
+    ) : name === 'Likes' ? (
+      active ? (
+        <HeartIconFilled />
+      ) : (
+        <HeartIcon />
+      )
+    ) : name === 'Profile' ? (
+      active ? (
+        <ProfileIconFilled />
+      ) : (
+        <ProfileIcon />
+      )
+    ) : name === 'Category' ? (
+      active ? (
+        <CategoryIconFilled />
+      ) : (
+        <CategoryIcon />
+      )
+    ) : (
+      <></>
+    );
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={screenOptions}
+      tabBar={props => {
+        if (props.state.index !== 1) {
+          return (
+            <View style={styles.container}>
+              {[...Array(5)].map((el, index) => (
+                <TouchableOpacity
+                  key={`${index}route`}
+                  style={styles.tabButton}
+                  disabled={
+                    props.state.routeNames[index] === 'Likes' ||
+                    props.state.routeNames[index] === 'Chats'
+                  }
+                  onPress={() =>
+                    props.navigation.navigate(props.state.routeNames[index])
+                  }>
+                  {iconRenderer({
+                    name: props.state.routeNames[index],
+                    active: index === props.state.index,
+                  })}
+                </TouchableOpacity>
+              ))}
+            </View>
+          );
+        }
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Category" component={HomeScreen} />
+      <Tab.Screen name="Search" component={HomeScreen} />
+      <Tab.Screen name="Likes" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTab;
