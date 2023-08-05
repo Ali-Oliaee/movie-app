9f150b623271463aa9fe8e1a1ce24dc7c8a7693e
ali <alioliaee31@gmail.com>
2023-08-05 19:00:55 +0330

    add folder structure

---
+++ src/components/HomeBanner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HomeBanner: React.FC = () => {
+  return (
+    <View>
+      <Text>HomeBanner</Text>
+    </View>
+  );
+};
+
+export default HomeBanner;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {HomeBanner} from '../../components';
 
 const HomeScreen: React.FC = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <HomeBanner />
     </View>
   );
 };
