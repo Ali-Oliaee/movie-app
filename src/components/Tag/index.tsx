409c4f8c63c74aa11e29fa625a36826c63240ae0
ali <alioliaee31@gmail.com>
2023-08-05 20:35:15 +0330

    add tag component

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Props from './types';
+import styles from './styles';
+import {Text, View} from 'react-native';
+import ImdbIcon from '../../assets/icons/ImdbIcon';
+
+const Tag: React.FC<Props> = ({title, isRate}) => {
+  return (
+    <View style={[styles.container, !title && styles.invisible]}>
+      <Text style={styles.title}>{title}</Text>
+      {isRate && <ImdbIcon />}
+    </View>
+  );
+};
+
+export default Tag;
