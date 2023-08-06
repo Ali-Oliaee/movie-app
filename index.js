ab76f19dba8206e3adf1c0a1e740047d7710fc8c
ali <alioliaee31@gmail.com>
2023-08-06 23:06:48 +0330

    add react-native-global-props

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
 import App from './src/App';
+import {Fonts} from './src/theme/fonts';
+import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import {setCustomText} from 'react-native-global-props';
+
+const customTextProps = {style: {color: 'white'}};
+setCustomText(customTextProps);
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/theme/fonts.ts
@@ -0,0 +1,5 @@
+export enum Fonts {
+  bold = 'Roboto-Bold',
+  regular = 'Roboto-Regular',
+  thin = 'Roboto-Thin',
+}
