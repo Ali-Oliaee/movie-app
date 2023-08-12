a0e7bc9200bc5b9c3f9679d2ef7c060bffc2ac3d
ali <alioliaee31@gmail.com>
2023-08-12 18:00:47 +0330

    add more movies

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Props from './types';
+import BannerProps from '@components/HomeBanner/types';
 import styles from './styles';
 import {homeCategory} from '@utils/genres';
 import AppLayout from '@layouts/AppLayout';
 import RightArrowIcon from '@assets/icons/right-arrow';
 import {AdsBox, HomeBanner, MovieCard} from '@components';
 import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {movies} from '@utils/movies';
 
 const HomeScreen: React.FC<Props> = ({navigation}) => {
-  const [activeCategory, setActiveCategory] = React.useState<number>(1);
+  const [activeCategory, setActiveCategory] =
+    React.useState<string>('Trending');
 
   return (
     <AppLayout>
@@ -18,11 +21,11 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
             <TouchableOpacity
               key={id}
               style={styles.categoryItem}
-              onPress={() => setActiveCategory(id)}>
+              onPress={() => setActiveCategory(title)}>
               <Text
                 style={[
                   styles.categoryItemText,
-                  id === activeCategory && styles.categoryItemTextActive,
+                  title === activeCategory && styles.categoryItemTextActive,
                 ]}>
                 {title}
               </Text>
@@ -30,40 +33,13 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
           ))}
         </ScrollView>
         <ScrollView horizontal>
-          <HomeBanner
-            image={require('@assets/images/jung_e.png')}
-            name="Jung_E"
-            kind="Movie"
-            description="This riveting sci-fi action-drama from “Hellbound” director Yeon Sang-ho stars Kang Soo-youn, Kim Hyun-joo and Ryu Kyung-soo."
-            genres={['Action', 'Sci-fi']}
-            year={2023}
-            age="+13"
-            rate={6.8}
-            onPress={() => navigation.navigate('Movie')}
-          />
-          <HomeBanner
-            image={require('@assets/images/oppenheimer.png')}
-            name="Oppenheimer"
-            kind="Movie"
-            description="J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion."
-            genres={['Thriller', 'Drama']}
-            year={2023}
-            age="+15"
-            isLiked
-            rate={8.8}
-            onPress={() => navigation.navigate('Movie')}
-          />
-          <HomeBanner
-            image={require('@assets/images/medellin.jpeg')}
-            name="Medellin"
-            kind="Movie"
-            description="In order to save his younger brother from the hands of narcos, Reda gathers a team to raid the kidnappers in Medellín, Colombia."
-            genres={['Action', 'Drama']}
-            year={2023}
-            age="+18"
-            rate={7.1}
-            onPress={() => navigation.navigate('Movie')}
-          />
+          {movies[activeCategory].map((props: BannerProps, index: number) => (
+            <HomeBanner
+              key={index}
+              {...props}
+              onPress={() => navigation.navigate('Movie')}
+            />
+          ))}
         </ScrollView>
         <AdsBox />
         <View style={styles.forYouBox}>
---
+++ src/utils/movies.ts
@@ -0,0 +1,247 @@
+export const movies = {
+  Trending: [
+    {
+      image: require('@assets/images/jung_e.png'),
+      name: 'Jung_E',
+      kind: 'Movie',
+      description:
+        'This riveting sci-fi action-drama from “Hellbound” director Yeon Sang-ho stars Kang Soo-youn, Kim Hyun-joo and Ryu Kyung-soo.',
+      genres: ['Action', 'Sci-fi'],
+      year: 2023,
+      age: '+13',
+      rate: 6.8,
+    },
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+    {
+      image: require('@assets/images/medellin.jpeg'),
+      name: 'Medellin',
+      kind: 'Movie',
+      description:
+        'In order to save his younger brother from the hands of narcos, Reda gathers a team to raid the kidnappers in Medellín, Colombia.',
+      genres: ['Action', 'Drama'],
+      year: 2023,
+      age: '+18',
+      rate: 7.1,
+    },
+  ],
+  New: [
+    {
+      image: require('@assets/images/barbie.png'),
+      name: 'Barbie',
+      kind: 'Movie',
+      description:
+        'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land.',
+      genres: ['Fantasy', 'Comedy'],
+      year: 2023,
+      age: '+12',
+      isLiked: false,
+      rate: 6.6,
+    },
+    {
+      image: require('@assets/images/transformers.png'),
+      name: 'Transformers',
+      kind: 'Movie',
+      description:
+        'When and Where Will Transformers One Be Released? Transformers One is still in production, and Paramount Pictures recently pushed back the release.',
+      genres: ['Action'],
+      year: 2024,
+      age: '+16',
+      isLiked: false,
+      rate: 6.9,
+    },
+    {
+      image: require('@assets/images/trolls.png'),
+      name: 'Trolls',
+      kind: 'Movie',
+      description:
+        'After the Bergens invade Troll Village, Poppy (Anna Kendrick), the happiest Troll ever born, and the overly-cautious, curmudgeonly Branch set off on a journey to rescue her friends.',
+      genres: ['Animation', 'Comedy'],
+      year: 2020,
+      age: '+3',
+      isLiked: true,
+      rate: 7.3,
+    },
+  ],
+  Movies: [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+    {
+      image: require('@assets/images/barbie.png'),
+      name: 'Barbie',
+      kind: 'Movie',
+      description:
+        'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land.',
+      genres: ['Fantasy', 'Comedy'],
+      year: 2023,
+      age: '+12',
+      isLiked: false,
+      rate: 6.6,
+    },
+    {
+      image: require('@assets/images/jung_e.png'),
+      name: 'Jung_E',
+      kind: 'Movie',
+      description:
+        'This riveting sci-fi action-drama from “Hellbound” director Yeon Sang-ho stars Kang Soo-youn, Kim Hyun-joo and Ryu Kyung-soo.',
+      genres: ['Action', 'Sci-fi'],
+      year: 2023,
+      age: '+13',
+      rate: 6.8,
+    },
+  ],
+  Series: [
+    {
+      image: require('@assets/images/breaking-bad.jpeg'),
+      name: 'Breaking Bad',
+      kind: 'Series',
+      description:
+        'Breaking Bad is an American crime drama television series created and produced by Vince Gilligan for AMC.',
+      genres: ['Family', 'Drama'],
+      year: 2012,
+      age: '+18',
+      isLiked: true,
+      rate: 9.9,
+    },
+    {
+      image: require('@assets/images/got.jpg'),
+      name: 'Game Of Thrones',
+      kind: 'Series',
+      description:
+        'George R.R. Martin\'s best-selling book series "A Song of Ice and Fire" is brought to the screen as HBO sinks its considerable storytelling teeth into the medieval fantasy epic',
+      genres: ['Fantasy', 'Drama'],
+      year: 2012,
+      age: '+18',
+      isLiked: true,
+      rate: 9.9,
+    },
+    {
+      image: require('@assets/images/office.jpeg'),
+      name: 'The Office',
+      kind: 'Series',
+      description:
+        'This US adaptation, set at a paper company in Scranton, Pa., has a similar documentary style to that of the Ricky Gervais-led British original.',
+      genres: ['Comedy'],
+      year: 2012,
+      age: '+13',
+      isLiked: true,
+      rate: 7.9,
+    },
+  ],
+  Animation: [
+    {
+      image: require('@assets/images/trolls.png'),
+      name: 'Trolls',
+      kind: 'Movie',
+      description:
+        'After the Bergens invade Troll Village, Poppy (Anna Kendrick), the happiest Troll ever born, and the overly-cautious, curmudgeonly Branch set off on a journey to rescue her friends.',
+      genres: ['Animation', 'Comedy'],
+      year: 2020,
+      age: '+3',
+      isLiked: true,
+      rate: 7.3,
+    },
+    {
+      image: require('@assets/images/spiderman.jpeg'),
+      name: 'Spider Man',
+      kind: 'Movie',
+      description:
+        "After reuniting with Gwen Stacy, Brooklyn's full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence.",
+      genres: ['Animation'],
+      year: 2023,
+      age: '+3',
+      isLiked: true,
+      rate: 7.3,
+    },
+  ],
+  'Tv Show': [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+  ],
+  Action: [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+  ],
+  Adventure: [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+  ],
+  Comedy: [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+  ],
+  Crime: [
+    {
+      image: require('@assets/images/oppenheimer.png'),
+      name: 'Oppenheimer',
+      kind: 'Movie',
+      description:
+        "J. Robert Oppenheimer led a team of scientists on the Manhattan World War II, resulting in the world's the first nuclear explosion.",
+      genres: ['Thriller', 'Drama'],
+      year: 2023,
+      age: '+15',
+      isLiked: true,
+      rate: 8.8,
+    },
+  ],
+};
